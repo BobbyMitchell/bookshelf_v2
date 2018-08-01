dc93d4eee25e1fea1e665226a780480ce89fda88
rob <robmitchell@email.com>
2018-08-01 17:57:29 +0200

    Users email and rating put in to show

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   has_many :user_books
   accepts_nested_attributes_for :user_books, allow_destroy: true
   has_many :users, through: :user_books
+  accepts_nested_attributes_for :users,  allow_destroy: true
   has_attachment :photo
   validates :title, :author, presence: true
 end
