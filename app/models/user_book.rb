f4a88f34ed66f0ebd498bd0634e5beee4302d584
rob <robmitchell@email.com>
2018-08-01 09:13:06 +0200

    Ave rating working. rating removed from book schema

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,4 +1,5 @@
 class UserBook < ApplicationRecord
   belongs_to :book
   belongs_to :user
+  #attr_accessor :rating, :book_id, :user_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730144643) do
+ActiveRecord::Schema.define(version: 20180801064649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20180730144643) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
-    t.integer "rating"
     t.boolean "have_read"
     t.string "genre"
     t.datetime "created_at", null: false
