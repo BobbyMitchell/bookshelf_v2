b31ad2860611114c2dc45499740691c7b4d7ba6e
rob <robmitchell@email.com>
2018-08-02 14:36:11 +0200

    username now working in devise

--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 20180802072817) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+
   end
 
   add_foreign_key "user_books", "books"
