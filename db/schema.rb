0037f7a8a33b8cf7497ba62be054066071de669a
rob <robmitchell@email.com>
2018-08-06 15:32:32 +0200

    created_by added to book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180802072817) do
+ActiveRecord::Schema.define(version: 20180806132805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180802072817) do
     t.bigint "isbn"
     t.integer "page_count"
     t.text "description"
+    t.bigint "created_by"
   end
 
   create_table "user_books", force: :cascade do |t|
@@ -72,7 +73,6 @@ ActiveRecord::Schema.define(version: 20180802072817) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
   add_foreign_key "user_books", "books"
